7fd0b6aab140752baaca6e9abcc64228ee2f3f35
Lezor <rawr-+4@live.com>
2020-05-31 23:09:51 +0100

    Cleanup and fix modscript name

--- ui/src/main/java/org/rspeer/ui/component/menu/BotMenuBar.java
+++ ui/src/main/java/org/rspeer/ui/component/menu/BotMenuBar.java
@@ -1,8 +1,10 @@
 package org.rspeer.ui.component.menu;
 
+import org.rspeer.commons.Pair;
 import org.rspeer.environment.Environment;
 import org.rspeer.game.Game;
 import org.rspeer.ui.component.menu.script.ScriptMenu;
+import org.rspeer.ui.debug.Debug;
 import org.rspeer.ui.debug.GameDebug;
 import org.rspeer.ui.debug.explorer.itf.InterfaceExplorer;
 import org.rspeer.ui.locale.Message;
@@ -11,8 +13,11 @@ import javax.swing.*;
 
 public class BotMenuBar extends JMenuBar {
 
+    private static final DebugEntry[] DEBUG_ENTRIES = {
+            new DebugEntry(new JCheckBoxMenuItem("Game"), new GameDebug())
+    };
+
     private final Environment environment;
-    private GameDebug gameDebug;
 
     public BotMenuBar(Environment environment) {
         this.environment = environment;
@@ -32,8 +37,8 @@ public class BotMenuBar extends JMenuBar {
     private JMenu createWindowMenu() {
         JMenu window = new JMenu(Message.WINDOW.getActive(environment.getPreferences()));
 
-        JCheckBoxMenuItem onTop = new JCheckBoxMenuItem(Message.ALWAYS_ON_TOP.getActive(environment.getPreferences()));
-        onTop.addItemListener(act -> environment.getBotContext().getFrame().setAlwaysOnTop(onTop.getState()));
+        JMenuItem onTop = new JCheckBoxMenuItem(Message.ALWAYS_ON_TOP.getActive(environment.getPreferences()));
+        onTop.addItemListener(act -> environment.getBotContext().getFrame().setAlwaysOnTop(onTop.isSelected()));
 
         window.add(onTop);
         return window;
@@ -45,21 +50,22 @@ public class BotMenuBar extends JMenuBar {
         JMenuItem itfs = new JMenuItem("Interfaces");
         itfs.addActionListener(act -> new InterfaceExplorer(environment));
 
-        JCheckBoxMenuItem renderScene = new JCheckBoxMenuItem("Render Scene", true);
-        renderScene.addActionListener(act -> Game.getClient().setSceneRenderingEnabled(!renderScene.isSelected()));
+        JMenuItem renderScene = new JCheckBoxMenuItem("Render Scene", true);
+        renderScene.addActionListener(act -> Game.getClient().setSceneRenderingDisabled(!renderScene.isSelected()));
 
-        JCheckBoxMenuItem renderGameDebug = new JCheckBoxMenuItem("Render Game Debug");
-        renderGameDebug.addActionListener(act -> {
-            if (renderGameDebug.isSelected()) {
-                setGameDebug(new GameDebug());
-                Game.getClient().getEventDispatcher().subscribe(getGameDebug());
-            } else {
-                Game.getClient().getEventDispatcher().unsubscribe(getGameDebug());
-                setGameDebug(null);
-            }
-        });
+        for (DebugEntry entry : DEBUG_ENTRIES) {
+            JMenuItem option = entry.getLeft();
+            Debug debugger = entry.getRight();
+            option.addActionListener(act -> {
+                if (option.isSelected()) {
+                    Game.getClient().getEventDispatcher().subscribe(debugger);
+                } else {
+                    Game.getClient().getEventDispatcher().unsubscribe(debugger);
+                }
+            });
+            debug.add(option);
+        }
 
-        debug.add(renderGameDebug);
         debug.add(renderScene);
         debug.add(itfs);
 
@@ -70,11 +76,10 @@ public class BotMenuBar extends JMenuBar {
         return environment;
     }
 
-    public GameDebug getGameDebug() {
-        return gameDebug;
-    }
+    private static class DebugEntry extends Pair<JMenuItem, Debug> {
 
-    public void setGameDebug(GameDebug gameDebug) {
-        this.gameDebug = gameDebug;
+        private DebugEntry(JMenuItem option, Debug debug) {
+            super(option, debug);
+        }
     }
 }
