38e362ecc9c48c9072a778d5b79ef842d295591c
zScorpio <el-zaiat@msn.com>
2020-06-12 20:40:32 +0200

    Register the top-level listener class

--- api/src/main/java/org/rspeer/event/EventDispatcher.java
+++ api/src/main/java/org/rspeer/event/EventDispatcher.java
@@ -1,11 +1,13 @@
 package org.rspeer.event;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
 import org.rspeer.event.listener.EventListener;
 
 public class EventDispatcher {
@@ -13,7 +15,7 @@ public class EventDispatcher {
     private final Map<Class<?>, List<EventListener>> listeners = new ConcurrentHashMap<>();
 
     public void subscribe(EventListener el) {
-        List<Class<?>> interfaces = getEventInterfaces(el.getClass());
+        List<Class<?>> interfaces = getListenerInterfaces(el.getClass());
         interfaces.forEach(group -> {
             if (!listeners.containsKey(group)) {
                 listeners.put(group, new CopyOnWriteArrayList<>());
@@ -23,7 +25,7 @@ public class EventDispatcher {
     }
 
     public void unsubscribe(EventListener el) {
-        List<Class<?>> interfaces = getEventInterfaces(el.getClass());
+        List<Class<?>> interfaces = getListenerInterfaces(el.getClass());
         interfaces.forEach(group -> {
             if (listeners.containsKey(group)) {
                 List<EventListener> listenersList = listeners.get(group);
@@ -42,25 +44,31 @@ public class EventDispatcher {
         listenersList.forEach(e::dispatch);
     }
 
-    private List<Class<?>> getEventInterfaces(Class<?> clazz) {
-        List<Class<?>> interfaces = new ArrayList<>();
+    private List<Class<?>> getListenerInterfaces(Class<?> clazz) {
+        List<Class<?>> listenerInterfaces = new ArrayList<>();
         if (clazz != null) {
-            // First we check the direct class interfaces
-            for (Class<?> interfase : clazz.getInterfaces()) {
-                // Check for the interface's SuperClass to determine if it extends EventListener
-                for (Class<?> superInterfase : interfase.getInterfaces()) {
-                    // If the interface extends EventListener then add it to the list
-                    if (superInterfase.equals(EventListener.class)) {
-                        if (!interfaces.contains(interfase)) {
-                            interfaces.add(interfase);
-                        }
-                        break;
-                    }
-                }
-            }
-            // Then we check the SuperClass interfaces
-            interfaces.addAll(getEventInterfaces(clazz.getSuperclass()));
+            // First we check the directly implemented interfaces
+            List<Class<?>> impl = Arrays.stream(clazz.getInterfaces())
+                                        .filter(this::isEventListenerInterface)
+                                        .collect(Collectors.toList());
+            listenerInterfaces.addAll(impl);
+            // Then we check interfaces implemented by the SuperClass
+            Class<?> superClass = clazz.getSuperclass();
+            listenerInterfaces.addAll(getListenerInterfaces(superClass));
         }
-        return interfaces;
+        return listenerInterfaces;
+    }
+
+    private boolean isEventListenerInterface(Class<?> clazz) {
+        if (clazz.isInterface()) {
+            // First we check if the class itself is an EventListener
+            if (clazz.equals(EventListener.class)) {
+                return true;
+            }
+            // Then we check if any of the SuperInterfaces is an EventListener
+            return Arrays.stream(clazz.getInterfaces())
+                         .anyMatch(this::isEventListenerInterface);
+        }
+        return false;
     }
 }
